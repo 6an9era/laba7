b27f0485d6dd5c737cc8c5c31cf20dbf9b4f43f5
6an9era <vanyafedya123@gmail.com>
2023-05-16 20:15:54 +0300

    Initial commit

---
+++ src/laba7ivan/Peer.java
@@ -0,0 +1,19 @@
+package laba7ivan;
+
+public class Peer{
+
+    private String name;
+    private String addres;
+
+    public Peer(String name, String addres) {
+        this.name = name;
+        this.addres = addres;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getAddres(){
+        return addres;
+    }
+}
